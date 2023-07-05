da2fba0c976e1758f607c746b0c01ccc97f3713d
Austin Isaac <vinuisaac@hotmail.com>
2023-07-05 11:55:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import auth, todos,users
+from starlette.staticfiles import StaticFiles
+import os
+print(os.getcwd())
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+static_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+# Mount the 'static' directory
+app.mount("/static", StaticFiles(directory=static_directory), name="static")
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
