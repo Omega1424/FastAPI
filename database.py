7a634bb73e78decc26c841d69128698d60b0cb2a
Omega1424 <vinuisaac@hotmail.com>
2023-07-06 18:42:05 +0800

    removed sqlite code

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "postgresql://cqyrtpid:is7Qf61UoK7OyeIF0E8zWxC31JfHqEEd@satao.db.elephantsql.com/cqyrtpid"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
