47964dce56dec10576b3d0005dc3f74c3077fad2
Omega1424 <vinuisaac@hotmail.com>
2023-07-06 18:32:44 +0800

    Changed database to postgresql

--- database.py
+++ database.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+SQLALCHEMY_DATABASE_URL = "postgresql://cqyrtpid:is7Qf61UoK7OyeIF0E8zWxC31JfHqEEd@satao.db.elephantsql.com/cqyrtpid"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
